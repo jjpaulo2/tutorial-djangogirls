916916e1253a9ef2a06d0b94bb69741b8cc25876
João Paulo Carvalho <jjjpaulo3@gmail.com>
2018-11-23 17:39:13 -0300

    finalizando blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("hello/", views.hello, name="hello")
+    path("postagens/", views.postagens, name="postagens")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Postagem
 
 # Create your views here.
 
-def index(request):
+def hello(request):
     return HttpResponse("<h3>Bem vindo(a) ao meu site!</h3>")
 
-def hello(request):
-    return render(request, "blog/hello.html", {})
+def index(request):
+    return render(request, "blog/index.html")
+
+def postagens(request):
+    postagens = Postagem.objects.order_by("data_publicacao")
+    return render(request, "blog/postagens.html", {"postagens": postagens})
